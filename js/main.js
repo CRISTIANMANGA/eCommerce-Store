e26a43dd6d98d3e22c5e7f9ded96a9a8f1e39fb3
CRISTIANMANGA <thecrishman@gmail.com>
2024-05-22 08:47:03 -0500

    fix :bug: arreglando errores

--- js/main.js
+++ js/main.js
@@ -1,24 +1,17 @@
-document.getElementById('pay-button').addEventListener('click', function() {
-    var paymentMethod = document.getElementById('payment-method').value;
-    switch (paymentMethod) {
-        case 'credit-card':
-            window.location.href = 'https://www.example.com/credit-card-payment';
-            break;
-        case 'paypal':
-            window.location.href = 'https://www.paypal.com';
-            break;
-        case 'stripe':
-            window.location.href = 'https://www.stripe.com';
-            break;
-        case 'bank-transfer':
-            window.location.href = 'https://www.example.com/bank-transfer-info';
-            break;
-        case 'cash-on-delivery':
-            window.location.href = 'https://www.example.com/cash-on-delivery-info';
-            break;
-        default:
-            // No se seleccionó ningún método de pago, maneja esto según sea necesario
-            break;
-    }
-});
+function searchProducts() {
 
+    var searchText = document.getElementById('search').value.toLowerCase();
+
+    var productos = document.querySelectorAll('.productCont');
+
+    productos.forEach(function(producto) {
+        var titulo = producto.querySelector('.productTitle').innerText.toLowerCase();
+        var descripcion = producto.querySelector('.productoDescrip').innerText.toLowerCase();
+
+        if (titulo.includes(searchText) || descripcion.includes(searchText) || searchText === '') {
+            producto.style.display = 'block';
+        } else {
+            producto.style.display = 'none';
+        }
+    });
+}
